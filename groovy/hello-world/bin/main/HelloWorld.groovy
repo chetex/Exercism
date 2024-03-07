53b308c4361ecf179487e889a627281204e303ba
chetex <chetex69@gmail.com>
2024-03-07 08:20:05 +0100

    Delete groovy. Added bash

--- groovy/hello-world/bin/main/HelloWorld.groovy
+++
@@ -1,7 +0,0 @@
-class HelloWorld {
-
-    def hello() {
-        'Hello, World!'
-    }
-
-}
--- python/ghost-gobble-arcade-game/arcade_game.py
+++ python/ghost-gobble-arcade-game/arcade_game.py
@@ -5,24 +5,26 @@
 # :param power_pellet_active: bool - does the player have an active power pellet?
 # :param touching_ghost: bool - is the player touching a ghost?
 def eat_ghost(power_pellet_active, touching_ghost):
-    return power_pellet_active and touching_ghost;
+    return power_pellet_active and touching_ghost
 
 # The function should return True if Pac-Man is touching a power pellet or a dot.
 def score(touching_power_pellet, touching_dot):
-    return touching_power_pellet or touching_dot;
+    return touching_power_pellet or touching_dot
 
 
+""" Trigger the victory event when all dots have been eaten. Define the lose() function that takes two parameters
+:param power_pellet_active: if Pac-Man has a power pellet active
+:param touching_ghost: if Pac-Man is touching a ghost
+:returns: Boolean value if Pac-Man wins
+"""
 def lose(power_pellet_active, touching_ghost):
-    return power_pellet_active and touching_ghost;
+    return power_pellet_active or touching_ghost
 
-
+""" Trigger the victory event when all dots have been eaten. Define the win() function that takes three parameters
+:param has_eaten_all_dots: if Pac-Man has eaten all of the dots
+:param power_pellet_active: if Pac-Man has a power pellet active
+:param touching_ghost: if Pac-Man is touching a ghost
+:returns: Boolean value if Pac-Man wins
+"""
 def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
-    """Trigger the victory event when all dots have been eaten.
-
-    :param has_eaten_all_dots: bool - has the player "eaten" all the dots?
-    :param power_pellet_active: bool - does the player have an active power pellet?
-    :param touching_ghost: bool - is the player touching a ghost?
-    :return: bool - has the player won the game?
-    """
-
-    pass
+    return has_eaten_all_dots and not lose(power_pellet_active, touching_ghost) 
