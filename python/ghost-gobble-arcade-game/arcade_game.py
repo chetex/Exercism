ba8bd7b1554d4d90028757e7c8b028e48043c1e2
chetex <chetex69@gmail.com>
2024-03-08 20:10:17 +0100

    Sring compilation error

--- python/ghost-gobble-arcade-game/arcade_game.py
+++ python/ghost-gobble-arcade-game/arcade_game.py
@@ -12,19 +12,17 @@ def score(touching_power_pellet, touching_dot):
     return touching_power_pellet or touching_dot
 
 
-""" Trigger the victory event when all dots have been eaten. Define the lose() function that takes two parameters
-:param power_pellet_active: if Pac-Man has a power pellet active
-:param touching_ghost: if Pac-Man is touching a ghost
-:returns: Boolean value if Pac-Man wins
-"""
+#Trigger the victory event when all dots have been eaten. Define the lose() function that takes two parameters
+# param power_pellet_active: if Pac-Man has a power pellet active
+# param touching_ghost: if Pac-Man is touching a ghost
+#returns: Boolean value if Pac-Man wins
 def lose(power_pellet_active, touching_ghost):
     return touching_ghost and not power_pellet_active
 
-""" Trigger the victory event when all dots have been eaten. Define the win() function that takes three parameters
-:param has_eaten_all_dots: if Pac-Man has eaten all of the dots
-:param power_pellet_active: if Pac-Man has a power pellet active
-:param touching_ghost: if Pac-Man is touching a ghost
-:returns: Boolean value if Pac-Man wins
-"""
+# Trigger the victory event when all dots have been eaten. Define the win() function that takes three parameters
+# param has_eaten_all_dots: if Pac-Man has eaten all of the dots
+# param power_pellet_active: if Pac-Man has a power pellet active
+# param touching_ghost: if Pac-Man is touching a ghost
+# returns: Boolean value if Pac-Man wins
 def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
     return has_eaten_all_dots and not lose(power_pellet_active, touching_ghost) 
